0c57baf9cf023e104d30ef99c28bfb60a1546ca1
RapidTheNerd <george.marr.cobc@gmail.com>
2017-06-22 00:32:44 +0100

    init commit

---
+++ main/java/me/rtn/poseidon/general/ChatIcons.java
@@ -0,0 +1,58 @@
+package me.rtn.poseidon.general;/*
+ * Jump
+ * Copyright (C) 2017 RapidTheNerd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+public class ChatIcons {
+    public static final String DARK_STAR = "★";
+    public static final String WHITE_STAR = "☆";
+    public static final String CIRCLE_BLANK_STAR = "✪";
+    public static final String BIG_BLOCK = "█";
+    public static final String SMALL_BLOCK = "▌";
+    public static final String SMALL_DOT = "•";
+    public static final String LARGE_DOT = "●";
+    public static final String HEART = "♥";
+    public static final String SMALL_ARROWS_RIGHT = "»";
+    public static final String SMALL_ARROWS_LEFT = "«";
+    public static final String ALERT = "⚠";
+    public static final String RADIOACTIVE = "☢";
+    public static final String BIOHAZARD = "☣";
+    public static final String PLUS = "✚";
+    public static final String BIG_HORIZONTAL_LINE = "▍";
+    public static final String SMALL_HORIZONTAL_LINE = "▏";
+    public static final String PLAY = "▶";
+    public static final String GOLD_ICON = "❂";
+    public static final String CROSS = "✖";
+    public static final String SLIM_CROSS = "✘";
+    public static final String BOXED_CROSS = "☒";
+    public static final String CHECKMARK = "✔";
+    public static final String BOXED_CHECKMARK = "☑";
+    public static final String LETTER = "✉";
+    public static final String BLACK_CHESS_KING = "♚";
+    public static final String BLACK_CHESS_QUEEN = "♛";
+    public static final String SKULL_AND_CROSSBONES = "☠";
+    public static final String WHITE_FROWNING_FACE = "☹";
+    public static final String WHITE_SMILING_FACE = "☺";
+    public static final String BLACK_SMILING_FACE = "☻";
+    public static final String PICK = "⛏";
+    public static final String SEPARATOR = "▬";
+    public static final String AEROPLANE = "✈";
+    public static final String TIMER = "⌛";
+    public static final String LIGHTNING_BOLT = "⚡";
+    public static final String SNOWFLAKE_1 = "❆";
+    public static final String SNOWFLAKE_2 = "❅";
+    public static final String NUKE = "☢";
+}
