0297212e1e5e660a8359a8ff511b879169303ade
RapidTheNerd <george.marr.cobc@gmail.com>
2017-06-22 01:09:16 +0100

    player join stuff - lammbdas not working for some reason?

--- main/java/me/rtn/poseidon/spigot/Main.java
+++ main/java/me/rtn/poseidon/spigot/Main.java
@@ -2,12 +2,17 @@ package me.rtn.poseidon.spigot;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+
+
 public final class Main extends JavaPlugin {
 
     private static Main spigotInstance;
     @Override
     public void onEnable() {
         spigotInstance = this;
+
+
+        //lambda streaming not working?!?!?!
     }
 
     @Override
---
+++ main/java/me/rtn/poseidon/spigot/listeners/PlayerJoin.java
@@ -0,0 +1,36 @@
+package me.rtn.poseidon.spigot.listeners;/*
+ * Jump
+ * Copyright (C) 2017 RapidTheNerd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import me.rtn.poseidon.bungee.Proxy;
+import me.rtn.poseidon.general.packets.ActionBar;
+import me.rtn.poseidon.general.packets.Title;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+public class PlayerJoin implements Listener {
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent event){
+        Player player = event.getPlayer();
+        Title.sendTitle(player, ChatColor.BLUE + "Welcome to the Poseidon network!");
+        ActionBar.sendActionBar(player, ChatColor.GREEN + "You are on " + Proxy.getProxyInstance() + "!");
+    }
+}
