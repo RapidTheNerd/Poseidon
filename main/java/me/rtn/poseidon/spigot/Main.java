76a03fc89fe416985acfe1e0e05818a223da5162
RapidTheNerd <george.marr.cobc@gmail.com>
2017-06-24 16:28:49 +0100

    not too sure why lambda streaming isn't working

--- main/java/me/rtn/poseidon/spigot/Main.java
+++ main/java/me/rtn/poseidon/spigot/Main.java
@@ -1,18 +1,24 @@
 package me.rtn.poseidon.spigot;
 
+import me.rtn.poseidon.spigot.listeners.PlayerJoin;
+import me.rtn.poseidon.spigot.managers.WhitelistManager;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 
-
-public final class Main extends JavaPlugin {
+public class Main extends JavaPlugin implements Listener {
 
     private static Main spigotInstance;
+
     @Override
     public void onEnable() {
         spigotInstance = this;
 
-
-        //lambda streaming not working?!?!?!
+        getServer().getPluginManager().registerEvents(new WhitelistManager(), this);
+        getServer().getPluginManager().registerEvents(new PlayerJoin(), this);
+        //lambda's hate me
     }
 
     @Override
